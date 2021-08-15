555b67f488dfcb50cfb3a67075a14694f4045f0b
Kaiymu <tom_1993@hotmail.fr>
2021-08-15 11:00:59 +0200

    [CODE] - Updating Pool system to include also a Singleton system.

---
+++ Runtime/Scripts/Singleton/SingleBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class SingleBehaviour<T> : MonoBehaviour where T : MonoBehaviour
+{
+	private static T _instance;
+
+	public static T instance {
+		get {
+
+			if (_instance == null)
+			{
+				_instance = FindObjectOfType(typeof(T)) as T;
+			}
+
+			if (_instance == null)
+			{
+				GameObject o = new GameObject("SingleBehaviour<" + typeof(T).ToString() + ">");
+				_instance = o.AddComponent<T>();
+			}
+
+			return _instance;
+		}
+	}
+}
